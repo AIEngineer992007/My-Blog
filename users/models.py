7ec6af1ba71543874850ea866228a4b9fca072a1
DangHai <dangnguyenngochai2021@gmail.com>
2026-09-21 23:22:02 +0700

    Remove default image 3

--- users/models.py
+++ users/models.py
@@ -1,22 +1,26 @@
+import os
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_image', height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(upload_to='profile_image', default='default.jpg')
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        
-        img = Image.open(self.image.path)
-        
-        if img.height > 300 and img.width > 300:
-            new_size = (300, 300)
-            img.thumbnail(new_size)
-            img.save(self.image.path)
 
+        # Kiểm tra an toàn: Có file đính kèm AND file thực sự tồn tại trên ổ đĩa
+        if self.image and hasattr(self.image, 'path') and os.path.exists(self.image.path):
+            try:
+                img = Image.open(self.image.path)
+                if img.height > 300 or img.width > 300:
+                    new_size = (300, 300)
+                    img.thumbnail(new_size)
+                    img.save(self.image.path)
+            except Exception as e:
+                # Bắt ngoại lệ nếu file ảnh bị hỏng hoặc không mở được
+                pass
