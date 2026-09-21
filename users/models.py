6d580f0bf5ea0c0352d04b5fa28e28b6ee254991
DangHai <dangnguyenngochai2021@gmail.com>
2026-09-21 23:06:08 +0700

    Remove default image

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile_image/default.jpg', upload_to='profile_image', height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(upload_to='profile_image', height_field=None, width_field=None, max_length=None)
 
     def __str__(self):
         return f'{self.user.username} Profile'
